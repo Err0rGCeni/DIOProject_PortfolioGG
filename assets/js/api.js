fb82f5243686597becce9cdaef97d4808ad0861b
GCeni <gceni10@gmail.com>
2023-07-10 19:31:50 -0300

    atualizado

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = ''
+    const url = 'https://raw.githubusercontent.com/Err0rGCeni/DIOProject_PortfolioGG/main/data/profile.json'
     const fetching = await fetch(url)
     return await fetching.json()
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,36 @@
+function updateProfileInfo(profileData) {
+    const photo = document.getElementById("profile-photo");
+    photo.src = profileData.photo;
+    photo.alt = profileData.name;
+
+    const name = document.getElementById('profile-name');
+    name.innerText = profileData.name;
+    const job = document.getElementById('profile-job');
+    job.innerText = profileData.job;
+    const location = document.getElementById('profile-location');
+    location.innerText = profileData.location;
+    const phone = document.getElementById('profile-phone');
+    phone.innerText = profileData.phone;
+    phone.href = profileData.phone;
+    const email = document.getElementById('profile-email');
+    email.innerText = profileData.email;
+    email.href = profileData.email;
+}
+
+function updateSoftSkills(profileData) {
+    const softSkills = document.getElementById('soft-skills');
+    softSkills.innerHTML = profileData.skills.softSkills.map(e => `<li>${e}</li>`).join('');
+}
+
+function updateHardSkills(profileData) {
+    const hardSkills = document.getElementById('hard-skills');
+    softSkills.innerHTML = profileData.skills.hardSkills.map(
+        e => `<li><img src="${e.logo}" alt="${e.name}" title="${e.name}"></li>`
+        ).join('');
+}
+
 (async () => {
     const profileData = await fetchProfileData()
-    console.log(profileData)
+    updateProfileInfo(profileData);
+    updateSoftSkills(profileData);
 })()
